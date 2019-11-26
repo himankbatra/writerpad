5c339b62eef4c88d1d738ced2c6736d0b6a433fa
Himank Batra <himank2010@gmail.com>
2019-11-26 17:49:43 +0530

    removed * import

--- src/main/java/com/xebia/fs101/writerpad/domain/Article.java
+++ src/main/java/com/xebia/fs101/writerpad/domain/Article.java
@@ -1,6 +1,13 @@
 package com.xebia.fs101.writerpad.domain;
 
-import javax.persistence.*;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
